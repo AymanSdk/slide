c93c86bfa98ffb86ed3f23e905d2feac320dc1ce
Aymansdk <ayman.sdk123@outlook.com>
2025-01-03 14:19:29 +0100

    Add SubscriptionPlan component for user subscription management

---
+++ src/components/global/subscription-plan/index.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  type: "FREE" | "PRO";
+  children: React.ReactNode;
+};
+
+export const SubscriptionPlan = ({ children, type }: Props) => {
+  // TODO : return the subscription plan of the user
+  return children;
+};
