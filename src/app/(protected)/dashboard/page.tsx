aa77cf9edcef92f8af786ca1db3dd168620d9881
Aymansdk <ayman.sdk123@outlook.com>
2025-01-03 12:58:09 +0100

    Add initial dashboard page component with server actions

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Page = async (props: Props) => {
+  // Server Actions
+
+  //   onBoarding
+
+  return <div>Page</div>;
+};
+
+export default Page;
